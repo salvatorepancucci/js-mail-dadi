35b98a71e79390b2e40b9fbd87fc6ddc8ecb3eb5
salvatorepancucci <salvatore.pancucci@gmail.com>
2024-06-28 16:24:22 +0200

    Added array within function

--- js/script.js
+++ js/script.js
@@ -1,7 +1,8 @@
 // Mail
 
 function checkEmail() {
-    
+    // Lista di chi può accedere
+    const allowedEmails = ['pippo@example.com', 'pluto@example.com', 'paperino@example.com', 'topolino@example.com'];
 }
 
 // Dice
