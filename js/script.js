d227cd60698924828328b633b6fa69a3bb7bcdb6
salvatorepancucci <salvatore.pancucci@gmail.com>
2024-06-28 16:24:55 +0200

    Added prompt for user data input

--- js/script.js
+++ js/script.js
@@ -3,6 +3,9 @@
 function checkEmail() {
     // Lista di chi può accedere
     const allowedEmails = ['pippo@example.com', 'pluto@example.com', 'paperino@example.com', 'topolino@example.com'];
+
+    // Chiediamo all'utente di inserire l'email
+    const userEmail = prompt('Inserisci la tua email:');
 }
 
 // Dice
